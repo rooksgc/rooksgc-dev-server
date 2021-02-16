90bf8e2aa686261d20bdc8207dc5bd07158b3715
aa.shestakov <aa.shestakov@tensor.ru>
2021-02-16 19:50:05 +0300

    postgres connection added

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import express from 'express'
 import config from 'config'
-const app = express()
-const PORT = config.get('port')
+import { Sequelize } from 'sequelize'
 import helloRoutes from './routes/hello'
 
-// todo fix type
+// app
+const app = express()
+
+// config
+const port = config.get('port')
+const dbuser = config.get('db.user')
+const dbpass = config.get('db.pass')
+const dbhost = config.get('db.host')
+const dbport = config.get('db.port')
+const dbname = config.get('db.name')
+
+// sequelize orm
+const sequelize = new Sequelize(
+  `postgres://${dbuser}:${dbpass}@${dbhost}:${dbport}/${dbname}`
+)
+
+// todo type fix
 app.use(express.json({ extended: true } as unknown))
 
 // routes
@@ -12,7 +27,10 @@ app.use('/api/hello', helloRoutes)
 
 async function start() {
   try {
-    app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`))
+    await sequelize.authenticate()
+    console.log('Postgres successfully connected')
+
+    app.listen(port, () => console.log(`Server listening on port ${port}...`))
   } catch (error) {
     console.log('Server error: ', error.message)
     process.exit(1)
