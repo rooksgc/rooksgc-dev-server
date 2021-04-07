62a128eec518f9916f85e6b8768bfdecea396874
aa.shestakov <aa.shestakov@tensor.ru>
2021-04-07 09:50:21 +0300

    handling errors in log

--- src/index.ts
+++ src/index.ts
@@ -16,10 +16,11 @@ const API_PATH = `${config.get('api.prefix')}/${config.get('api.version')}`
 const app = express()
 const router = Router()
 const server = createServer(app)
-const io = new Server(server, {
-  pingInterval: 30000,
-  pingTimeout: 60000
-})
+// const ioConfig = {
+//   pingInterval: 25000,
+//   pingTimeout: 60000
+// }
+const io = new Server(server)
 
 useSockets(io)
 
--- src/services/sockets.ts
+++ src/services/sockets.ts
@@ -1,11 +1,21 @@
 import { Server } from 'socket.io'
 import logger from './logger'
 
+const makeDate = () => {
+  const date = new Date(Date.now())
+  const hour = date.getHours()
+  const min = date.getMinutes()
+  const sec = date.getSeconds()
+  const ms = date.getMilliseconds()
+
+  return `${hour}:${min}:${sec}:${ms}`
+}
+
 const useSockets = (io: Server): void => {
   io.on('connection', (socket) => {
     const { id } = socket
     // eslint-disable-next-line no-console
-    console.log(`Client ${id} connected`)
+    logger.error(`[${makeDate()}] ${id} connected`)
 
     socket.on('channels:subscribe', (channelsList) => {
       if (!Array.isArray(channelsList) && !channelsList.length) return
@@ -25,24 +35,18 @@ const useSockets = (io: Server): void => {
     socket.on('channel:leave', (channel) => {
       socket.leave(channel)
       // eslint-disable-next-line no-console
-      console.log(`Client ${id} leaved channel: ${channel}`)
+      console.log(`${id} leaved channel: ${channel}`)
     })
 
     socket.on('disconnect', (reason: string) => {
-      const date = new Date(Date.now())
-      const hour = date.getHours()
-      const min = date.getMinutes()
-      const sec = date.getSeconds()
-      const ms = date.getMilliseconds()
-
       logger.error(
-        `[${hour}:${min}:${sec}:${ms}] [${socket.id}] disconnected. Reason:  ${reason}`
+        `[${makeDate()}] [${socket.id}] disconnected. Reason:  ${reason}`
       )
       // if (socket.io.connecting.indexOf(socket) === -1) {
       //   socket.connect()
       // }
       // eslint-disable-next-line no-console
-      console.log('Client desconnected!')
+      console.log(`${id} desconnected!`)
     })
   })
 }
