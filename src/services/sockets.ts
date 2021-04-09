908697027dda44c7a7bf2a2d9ce473082d443aa5
aa.shestakov <aa.shestakov@tensor.ru>
2021-04-09 16:47:01 +0300

    fix

--- src/services/sockets.ts
+++ src/services/sockets.ts
@@ -11,20 +11,24 @@ const makeDate = () => {
 }
 
 const useSockets = (io: Server): void => {
+  const subscribeToChannels = (socket, channelsList) => {
+    const { id } = socket
+    const date = makeDate()
+
+    logger.info(`[${date}] ${id} channelsList.length: ${channelsList.length}`)
+
+    if (!Array.isArray(channelsList) && !channelsList.length) return
+    channelsList.forEach((channel) => {
+      socket.join(channel.toString())
+    })
+    logger.info(`[${date}] ${id} subscribed to channels`)
+  }
   io.on('connection', (socket) => {
     const { id } = socket
-    // eslint-disable-next-line no-console
-    console.log(`[${makeDate()}] ${id} connected`)
     logger.info(`[${makeDate()}] ${id} connected`)
 
     socket.on('channels:subscribe', (channelsList) => {
-      const date = makeDate()
-      logger.info(`[${date}] ${id} channelsList.length: ${channelsList.length}`)
-      if (!Array.isArray(channelsList) && !channelsList.length) return
-      channelsList.forEach((channel) => {
-        socket.join(channel.toString())
-      })
-      logger.info(`[${date}] ${id} subscribed to channels`)
+      subscribeToChannels(socket, channelsList)
     })
 
     socket.on('channel:message:add', ({ activeChannelId, message }) => {
@@ -37,33 +41,14 @@ const useSockets = (io: Server): void => {
 
     socket.on('channel:leave', (channel) => {
       socket.leave(channel)
-      // eslint-disable-next-line no-console
-      console.log(`${id} leaved channel: ${channel}`)
       logger.info(`${id} leaved channel: ${channel}`)
-    })
-
-    socket.on('reconnecting', () => {
-      // eslint-disable-next-line no-console
-      logger.info(`[${makeDate()}] ${id} reconnecting`)
-    })
-
-    socket.on('reconnect_error', (error) => {
-      // eslint-disable-next-line no-console
-      console.log(`[${makeDate()}] ${id} reconnection`, error)
-      logger.info(`[${makeDate()}] ${id} reconnection error ${error.message}`)
-    })
-
-    socket.on('reconnect_failed', () => {
-      // eslint-disable-next-line no-console
-      console.log(`[${makeDate()}] ${id} reconnection failed`)
-      logger.info(`[${makeDate()}] ${id} reconnection failed`)
     })
 
     socket.on('disconnect', (reason: string) => {
       const date = makeDate()
       const message = `[${date}] ${id} disconnected: ${reason}`
       // eslint-disable-next-line no-console
-      console.log(message)
+      logger.info(`[${makeDate()}] ${id} disconnected: ${message}`)
     })
   })
 }
