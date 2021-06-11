82fdf1bc28943d762aa018f6eb3628b650697302
aa.shestakov <aa.shestakov@tensor.ru>
2021-06-11 15:34:03 +0300

    add auth login tests with supertest

--- src/routes/auth.test.ts
+++ src/routes/auth.test.ts
@@ -1,23 +1,56 @@
 import request from 'supertest'
-import { app } from '../index'
+import { app, API_PATH } from '../index'
 
 describe('Auth routes', () => {
-  it('Should handle /auth/login request with correct credentials', async (done) => {
-    const email = 'rooksgc@gmail.com'
-    const password = '1q1q2wAZSS'
+  describe('login', () => {
+    it('correct credentials', async () => {
+      const email = 'demo@gmail.com'
+      const password = '1212qQ'
 
-    await request(app)
-      .post('/auth/login')
-      .send({ email, password })
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(
-        200,
-        {
-          type: 'success',
-          message: 'Успешный вход в систему!'
-        },
-        done
+      const response = await request(app)
+        .post(`${API_PATH}/auth/login`)
+        .send({ email, password })
+        .expect('Content-Type', /json/)
+        .expect(200)
+
+      expect(response.body.type).toBe('success')
+      expect(response.body.message).toBe('Успешный вход в систему!')
+      expect(response.body.token).toBeDefined()
+      expect(response.body.data).toBeDefined()
+    })
+
+    it('wrong email', async () => {
+      const email = 'wrongusername@gmail.com'
+      const password = '12qwerDS'
+
+      const response = await request(app)
+        .post(`${API_PATH}/auth/login`)
+        .send({ email, password })
+        .expect('Content-Type', /json/)
+        .expect(409)
+
+      expect(response.body.type).toBe('error')
+      expect(response.body.message).toBe(
+        'Пользователя с таким email не существует'
       )
+      expect(response.body.token).not.toBeDefined()
+      expect(response.body.data).not.toBeDefined()
+    })
+
+    it('wrong password', async () => {
+      const email = 'demo@gmail.com'
+      const password = '12qwSA6'
+
+      const response = await request(app)
+        .post(`${API_PATH}/auth/login`)
+        .send({ email, password })
+        .expect('Content-Type', /json/)
+        .expect(401)
+
+      expect(response.body.type).toBe('error')
+      expect(response.body.message).toBe('Неверный пароль')
+      expect(response.body.token).not.toBeDefined()
+      expect(response.body.data).not.toBeDefined()
+    })
   })
 })
